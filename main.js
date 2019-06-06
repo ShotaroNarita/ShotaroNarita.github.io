1dbda903830df74344e7e91e895aeeb9a0033f71
Shotaro Narita <shotaro.narita@icloud.com>
2019-06-06 23:32:26 +0900

    add linetracer2

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-let canvas, context;
+let canvas, context, img, width, height;
 
 function init(){
     canvas = document.getElementById('canvas');
     context = canvas.getContext('2d');
 
     context.fillStyle='aliceblue';
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    width = canvas.width;
+    height = canvas.height;
+    context.fillRect(0, 0, width, height);
 
-    var img = new Image();
-    img.src = 'https://www.tam-tam.co.jp/tipsnote/wpdata/wp-content/uploads/2017/10/canvas_image.jpg';
+    img = new Image();
+    img.src = 'https://shotaronarita.github.io/linetemplate.png';
 }
 
 let x = 100;
@@ -17,14 +19,18 @@ let theta = 0;
 
 let h = 30;
 
-let dr = 1;
+let dr = 0;
 let dl = 3;
+
+let sl = 10;
+
+let censorL = {'x':0, 'y':0};
+let censorC = {'x':0, 'y':0};
+let censorR = {'x':0, 'y':0};
 
 function loop()
 {
-
-    context.fillStyle='aliceblue';
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(img, 0, 0, width, height);
     
     let ll = dl;
     let lr = dr;
@@ -34,22 +40,22 @@ function loop()
 
     if(ll == lr){
         R = 0;
+
+        x += ll * Math.cos(theta);
+        y += ll * Math.sin(theta);
     }else{
         R = (ll + lr) / (ll - lr) * h;
+
+        d_theta = (ll - lr) / (2 * h);
+
+        D = 2 * R * Math.sin((ll - lr) / 4);
+    
+        x += (- R * Math.sin(theta) + R * Math.cos(Math.PI / 2 - theta - d_theta));
+        y += (  R * Math.cos(theta) - R * Math.sin(Math.PI / 2 - theta - d_theta));
     }
-
-    d_theta = (ll - lr) / (2 * h);
-
-    // console.log('R = ', R, 'd_theta = ', d_theta, 'x = ', x, 'y = ', y);
-
-    D = 2 * R * Math.sin((ll - lr) / 4);
-
-    x += (- R * Math.sin(theta) + R * Math.cos(Math.PI / 2 - theta - d_theta));
-    y += (  R * Math.cos(theta) - R * Math.sin(Math.PI / 2 - theta - d_theta));
 
     theta += d_theta;
     
-
     let s = h * Math.sin(theta);
     let c = h * Math.cos(theta);
 
@@ -75,10 +81,51 @@ function loop()
     context.closePath();  
     context.fillStyle='red';
     context.fill();  
+
+    censorC.x = x + c;
+    censorC.y = y + s;
+
+    censorL.x = censorC.x + Math.sin(theta) * sl;
+    censorL.y = censorC.y - Math.cos(theta) * sl;
+
+    censorR.x = censorC.x - Math.sin(theta) * sl;
+    censorR.y = censorC.y + Math.cos(theta) * sl;
+
+    context.beginPath();
+    context.arc(censorC.x, censorC.y, 3, 0, Math.PI * 2, true);
+    context.closePath();  
+    context.fillStyle='blue';
+    context.fill();  
+
+    context.beginPath();
+    context.arc(censorL.x, censorL.y , 3, 0, Math.PI * 2, true);
+    context.closePath();  
+    context.fillStyle='blue';
+    context.fill();  
+
+    context.beginPath();
+    context.arc(censorR.x, censorR.y, 3, 0, Math.PI * 2, true);
+    context.closePath();  
+    context.fillStyle='blue';
+    context.fill();  
+
+    censorL.x = parseInt(censorL.x);
+    censorL.y = parseInt(censorL.y);
+    censorC.x = parseInt(censorC.x);
+    censorC.y = parseInt(censorC.y);
+    censorR.x = parseInt(censorR.x);
+    censorR.y = parseInt(censorR.y);
+
+    // let info = context.getImageData(100, 100, 1, 1);
+    let info = context.getImageData(censorL.x, censorL.y, 1, 1);
+    // console.log(censorL.x, censorL.y);
+    console.log(info.data);
+    document.getElementById('terminal').innerText = info.data;
+    //info.data;
+    
 }
 
 function main(){
     init();
     setInterval(loop, 100);
-    // context.fillRect(100, 100, 100, 100);
 }
